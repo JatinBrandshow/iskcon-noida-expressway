b8ae208cc80b8e7047d90e1f6a98c009eb12ac08
Shivam060 <yadavshiv633@gmail.com>
2026-03-12 09:58:09 +0530

    New Changes

---
+++ src/app/HomePage.js
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+// import MainSection from "@/components/MainSection";
+// import About from "@/components/About";
+// import EventsSection from "@/components/EventSection";
+// import Birthday from "@/components/Birthday";
+// import GratitudeSection from "@/components/GratitudeSection";
+
+const HomePage = () => {
+    return (
+        <>
+            {/* <MainSection />
+            <About />
+            <EventsSection />
+            <Birthday />
+            <GratitudeSection /> */}
+        </>
+    );
+};
+
+export default HomePage;
